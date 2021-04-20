0dde4da1a59edbe4ba87f1cea1b0c29406c7107a
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-20 11:51:47 -0700

    creating the home component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
