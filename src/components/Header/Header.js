7668fd390657a0ff7b8333ab2ea555fc112bad89
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-20 12:04:56 -0700

    structuring project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,3 @@
-import { IconButton } from '@material-ui/core';
 import React from 'react';
 import './Header.css';
 
@@ -6,14 +5,13 @@ function Header() {
     return (
         <div className="header">
             <div className="header__left">
-                <IconButton>
-                    <MenuIcon />
-                </IconButton>
-                <img
-                    src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
-                    alt="" />
             </div>
+            <div className="header__middle">
 
+            </div>
+            <div className="header__right">
+
+            </div>
         </div>
     )
 }
