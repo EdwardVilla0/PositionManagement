ca289eae1db02b4f36d9eca22ad5eb9a0a09cc17
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-21 12:15:49 -0700

    adding avatar and menu icon to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu'
 import React from 'react';
 import './Header.css';
 
@@ -5,12 +7,17 @@ function Header() {
     return (
         <div className="header">
             <div className="header__left">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
             </div>
             <div className="header__middle">
 
             </div>
             <div className="header__right">
-
+                <IconButton >
+                    <Avatar src="" />
+                </IconButton>
             </div>
         </div>
     )
