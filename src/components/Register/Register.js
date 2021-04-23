141fe47cb4ed94fe8f58a3a7017c4ca7f4858b95
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-23 16:39:46 -0700

    creating register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -7,14 +7,14 @@ function Register() {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [name, setName] = useState("");
     const [profilePic, setProfilePic] = useState("");
+
     return (
         <div className="register">
             <form>
-                <input value={name} onChange={e => setName(e.target.value)} placeholder="Full Name (required if registering)" type="text" />
-                <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" type="text" />
-                <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" />
-                <input value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Confirm Password" type="password" />
-
+                <input value={name} onChange={e => setName(e.target.value)} placeholder="Full Name (required if registering)" type="text" required />
+                <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" type="email" required />
+                <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password" required />
+                <input value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Confirm Password" type="password" required />
                 {/* <button type="submit" onClick={loginToApp}>Sign In</button> */}
                 <button type="submit" >Sign In</button>
             </form>
