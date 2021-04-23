53066dca73242831494495ff18cd512b5bcb5e2e
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-23 14:05:12 -0700

    installing firebase for auth

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCD_qhXM3wuayZ80eCKL69tujncH0e9q9w",
+    authDomain: "position-management-180c6.firebaseapp.com",
+    projectId: "position-management-180c6",
+    storageBucket: "position-management-180c6.appspot.com",
+    messagingSenderId: "1078697890024",
+    appId: "1:1078697890024:web:92d2a2bbb44965fd76f95d",
+    measurementId: "G-V2PQZ7XTV4"
+};
