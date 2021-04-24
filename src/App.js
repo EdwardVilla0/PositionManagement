65a655267d3d2b090813ee24ddeb9a3c6b67cd83
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-04-23 17:57:22 -0700

    creating logic to log in and out

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 
 function App() {
-  let x = false;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+        }))
+      } else {
+        dispatch(logout());
+      }
+    })
+
+
+  }, [])
+
+
   return (
     <Router>
 
-      {!x ? (
+      {!user ? (
         <Login />
         // <Register />
       ) : (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import { Avatar, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu'
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../../features/userSlice';
+import { auth } from '../../firebase';
 import Accounts from '../Accounts/Accounts';
 import Benefits from '../Benefits/Benefits';
 import Employees from '../Employees/Employees';
@@ -10,6 +13,12 @@ import Timesheets from '../Timesheets/Timesheets';
 import './Header.css';
 
 function Header() {
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch()
+    const logoutOfApp = () => {
+        dispatch(logout())
+        auth.signOut()
+    }
     return (
         <div className="header">
             <div className="header__left">
@@ -26,8 +35,10 @@ function Header() {
                 <Miscellaneous />
             </div>
             <div className="header__right">
-                <IconButton >
-                    <Avatar src="" />
+                <IconButton onClick={logoutOfApp}>
+                    <Avatar src="" >
+                        {user?.email[0]}
+                    </Avatar>
                 </IconButton>
             </div>
         </div>
