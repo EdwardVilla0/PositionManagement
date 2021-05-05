0dcc13fd5648b55a5972506b77c68d935f85e0a9
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-05-05 11:41:51 -0700

    adding routes to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Accounts from './components/Accounts/Accounts';
+import Benefits from './components/Benefits/Benefits';
 import Employees from './components/Employees/Employees';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Miscellaneous from './components/Miscellaneous/Miscellaneous';
+import PositionControl from './components/PositionControl/PositionControl';
 import Register from './components/Register/Register';
+import Timesheets from './components/Timesheets/Timesheets';
 import { login, logout, selectUser } from './features/userSlice';
 import { auth } from './firebase';
 
@@ -53,11 +58,25 @@ function App() {
           <Header />
           <Switch>
             <Route path="/" exact>
-
               <Home />
             </Route>
             <Route path="/employees" exact>
               <Employees />
+            </Route>
+            <Route paht='/positioncontrol'>
+              <PositionControl />
+            </Route>
+            <Route paht='/timesheets'>
+              <Timesheets />
+            </Route>
+            <Route paht='/accounts'>
+              <Accounts />
+            </Route>
+            <Route paht='/benefits'>
+              <Benefits />
+            </Route>
+            <Route paht='/miscellaneous'>
+              <Miscellaneous />
             </Route>
           </Switch>
           <Footer />
