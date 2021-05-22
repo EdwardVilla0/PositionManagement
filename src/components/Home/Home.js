590a7e980e3e684cef6347afa8c2f7bef18734f8
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-05-21 18:05:24 -0700

    adding to the employees page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,12 +5,13 @@ import './Home.css';
 function Home() {
 
     const myObj = {
-        id: 1,
+        id: 1020,
         firstName: 'John',
         lastName: 'Doe',
         sickTime: 10,
         personalTime: 10,
-        vacationTime: 8
+        vacationTime: 8,
+        birthday: "03/25/2015"
 
     };
 
@@ -18,11 +19,15 @@ function Home() {
         <div className="home">
             <div className="home__left">
                 <h1>My Timesheet</h1>
-                <h2>{myObj.sickTime}</h2>
+                <h2>sick time: {myObj.sickTime}</h2>
+                <h2>vacation time: {myObj.vacationTime}</h2>
+                <h2>personal time: {myObj.personalTime}</h2>
             </div>
             <div className="home__right">
                 <h1>My information</h1>
-                <h2>{myObj.firstName}</h2>
+                <h2>id: {myObj.id}</h2>
+                <h2>Full name: {myObj.firstName} {myObj.lastName}</h2>
+                <h2>birthday: {myObj.birthday}</h2>
             </div>
 
         </div>
